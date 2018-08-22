e39e6b02b2c6baaf5b0d23cc73406381a7c2f63e
Julien Cousineau <julien.cousineau@gmail.com>
2018-08-22 18:16:20 +0000

    build last edition

--- build/util.js
+++ build/util.js
@@ -72,79 +72,118 @@ if (!Number.prototype.ordermag) {
     
 }
 
-// Array : Create simple range
-if (!Array.prototype.range) {
-    Array.prototype.range = function() {
-        const len = this.length;
-        for(let i=0;i<len;i++)this[i]=i;
-        return this;
-    };
-}
+[Array,Int8Array,Int16Array, Int32Array,Uint8Array,Uint16Array, Uint32Array,Float32Array].forEach(item=>{
+    if (!item.prototype.range) {
+        item.prototype.range = function() {
+            for(let i=0;i<this.length;i++)this[i]=i;
+            return this;
+        };
+    }
+    
+    if (!item.prototype.clamp) {
+        item.prototype.clamp = function(min, max) {
+            for(let i=0;i<this.length;i++)this[i]=i.clamp(min,max);
+            return this;
+        };
+    }
+
+    if (!item.prototype.min) {
+        item.prototype.min = function(){
+            let min = +Infinity,len = this.length;
+            for (let i=0 ; i < len; i++ )
+                if ( this[i] < min ) min = this[i];
+            return min;
+        };
+    }    
+    
+    if (!item.prototype.max) {
+        item.prototype.max = function(){
+            let max = -Infinity, len = this.length;
+            for (let i=0 ; i < len; i++ )
+                if ( this[i] > max ) max = this[i];
+            return max;
+        };
+    }   
+    
+    if (!item.prototype.add) {
+        item.prototype.add = function(value){
+            for(let i=0,n=this.length;i<n;i++)this[i]+=value;
+            return this;
+        };
+    }
+    
+    if (!item.prototype.subtract) {
+        item.prototype.subtract = function(value){
+            for(let i=0,n=this.length;i<n;i++)this[i]-=value;
+            return this;
+        };
+    }
+    if (!item.prototype.multiply) {
+        item.prototype.multiply = function(value){
+            for(let i=0,n=this.length;i<n;i++)this[i]*=value;
+            return this;
+        };
+    }
+    
+    if (!item.prototype.divide) {
+        item.prototype.divide = function(value){
+            for(let i=0,n=this.length;i<n;i++)this[i]/=value;
+            return this;
+        };
+    }        
+    
+    if (!item.prototype.compare) {
+        item.prototype.compare = function( a ) {
+            const epsilon = 1.0E-7;
+            for (var i = 0, n = this.length; i<n; i++) {
+                if (a[i] - this[i] > epsilon) return false;
+            }
+            return true;
+        };        
+    }
+
+    
+    
+});
 
 
 
-// Array : Calculate max value
-if (!Array.prototype.max) {
-    Array.prototype.max = function(){
-        var max = -Infinity, len = this.length;
-        for (let i=0 ; i < len; i++ )
-            if ( this[i] > max ) max = this[i];
-        return max;
-    };
-}
+// // Create simple range
+// module.exports.range = function (n,type){
+//      n = (typeof n !== 'undefined') ?  n : 0;
+//      if (!(Number.isInteger(n))) throw Error("Error in range: Value must be an integer");
+//     return Array.from(new Array(n), function(x,i) {return i;});
+// };
 
-// Array : Calculate min value
-if (!Array.prototype.min) {
-    Array.prototype.min = function(){
-        let min = +Infinity,len = this.length;
-        for (let i=0 ; i < len; i++ )
-            if ( this[i] < min ) min = this[i];
-        return min;
-    };
-}
-
-// Float32Array : Calculate max value
-if (!Float32Array.prototype.max) {
-    Float32Array.prototype.max = function(){
-        let max = -Infinity, len = this.length;
-        for (let i=0 ; i < len; i++ )
-            if ( this[i] > max ) max = this[i];
-        return max;
-    };
-}
-
-// Float32Array : Calculate min value
-if (!Float32Array.prototype.min) {
-    Float32Array.prototype.min = function(){
-        let min = +Infinity,len = this.length;
-        for (let i=0 ; i < len; i++ )
-            if ( this[i] < min ) min = this[i];
-        return min;
-    };
-}
-
-// Float32Array : Clamp number between min and max
-if (!Float32Array.prototype.clamp) {
-    Float32Array.prototype.clamp = function(min, max) {
-        for(let i=0;i<this.length;i++)this[i]=i.clamp(min,max);
-        return this;
-    };
-}
-
-// Float32Array : Create simple range
-if (!Float32Array.prototype.range) {
-    Float32Array.prototype.range = function() {
-        for(let i=0;i<this.length;i++)this[i]=i;
-        return this;
-    };
-}
-
-
-
-// Create simple range
-module.exports.range = function (n){
-    return Array.from(new Array(n), function(x,i) {return i;});
+module.exports.range = function(n,type) {
+    n = (typeof n !== 'undefined') ?  n : 0;
+    if (!(Number.isInteger(n))) throw Error("Error in range: Value must be an integer");
+    let array;
+    
+    if(type=='Uint8')  array = new Uint8Array(n);
+    if(type=='Uint16') array = new Uint16Array(n);
+    if(type=='Uint32') array = new Uint32Array(n);
+    if(type=='Int8')  array = new Int8Array(n);
+    if(type=='Int16') array = new Int16Array(n);
+    if(type=='Int32') array = new Int32Array(n);
+    if(type=='Float32')  array = new Float32Array(n);
+    if((typeof type === 'undefined') || !array)array = new Array(n);
+    
+    for(let i=0;i<n;i++)array[i]=i;
+    return array;
 };
+
+
+
+exports.isFloat32Array = function( value ) {
+    return Object.prototype.toString.call( value ) === '[object Float32Array]';
+}; 
+exports.isUint32Array = function( value ) {
+    return Object.prototype.toString.call( value ) === '[object Uint32Array]';
+};
+exports.isArray = function( value ) {
+    return Object.prototype.toString.call( value ) === '[object Array]';
+}; 
 
 // Debounce function
 module.exports.debounce=function(func, wait, immediate) {
